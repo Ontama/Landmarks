da90682d0e79d4a209e5e6eea73c934b5b5a410f
ontama154 <onchanvvv@gmail.com>
2022-03-17 20:23:19 +0900

    チュートリアル　パスとシェイプの描画途中まで

---
+++ Shared/BadgeBackground.swift
@@ -0,0 +1,38 @@
+//
+//  BadgeBackground.swift
+//  Landmarks
+//
+//  Created by tomoyo_kageyama on 2022/03/16.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Path { path in
+            var width: CGFloat = 100.0
+            let height = width
+            path.move(
+                to: CGPoint(
+                    x: width * 0.95,
+                    y: height * 0.20
+                )
+            )
+            HexagonParameters.segments.forEach { segment in
+                path.addLine(
+                    to: CGPoint(
+                        x: width * segment.line.x,
+                        y: height * segment.line.y
+                    )
+                )
+            }
+        }
+        .fill(.black)
+    }
+}
+
+struct BadgeBackground_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeBackground()
+    }
+}
